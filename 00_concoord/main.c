a6990b7b887372ec158bd1c40de089f81cccdedb
computermouth <computermouth@crunchbangplusplus.org>
2018-03-19 22:26:29 -0700

    Scrolled window sort of works

--- 00_concoord/main.c
+++ 00_concoord/main.c
@@ -3,6 +3,7 @@
 static void destroy(GtkWidget*, gpointer);
 static gpointer cc_new_window();
 static gpointer cc_new_anim_pane();
+static gpointer cc_new_anim_scrolled();
 static gpointer cc_new_frame_pane();
 static gpointer cc_new_canvas_pane();
 
@@ -63,14 +64,47 @@ static gpointer cc_new_anim_pane(){
 	
 	GtkWidget *tmp_anim_pane = gtk_hpaned_new();
 	
-	GtkWidget *anim_add_button = gtk_button_new_with_label("Add Anim");
-	gtk_widget_set_size_request(anim_add_button, 100, 500);
+	//~ GtkWidget *anim_add_button = gtk_button_new_with_label("Add Anim");
+	//~ gtk_widget_set_size_request(anim_add_button, 100, 500);
+	GtkWidget *anim_scrolled = cc_new_anim_scrolled();
 	GtkWidget *frame_pane = cc_new_frame_pane();
 	
-	gtk_paned_add1(GTK_PANED(tmp_anim_pane), anim_add_button);	
+	gtk_paned_add1(GTK_PANED(tmp_anim_pane), anim_scrolled);	
 	gtk_paned_add2(GTK_PANED(tmp_anim_pane), frame_pane);
 	
 	return tmp_anim_pane;
+}
+
+//    +-------------------------+
+//    | +||                     |
+//    |   |                     |
+//    |   |                     |
+//    |   |                     |
+//    |   |                     |
+//    |   |                     |
+//    |   |                     |
+//    |   |                     |
+//    +-------------------------+
+
+static gpointer cc_new_anim_scrolled(){
+	
+	GtkWidget *tmp_anim_scrolled = gtk_scrolled_window_new(NULL, NULL);
+	gtk_container_set_border_width(GTK_CONTAINER(tmp_anim_scrolled), 5);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(tmp_anim_scrolled),
+		GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	
+	GtkWidget *table = gtk_table_new(10, 1, TRUE);
+	GtkWidget *anim_add_button0 = gtk_button_new_with_label("Anim");
+	GtkWidget *anim_add_button1 = gtk_button_new_with_label("Anim");
+	GtkWidget *anim_add_button2 = gtk_button_new_with_label("+");
+	
+	gtk_table_attach_defaults(GTK_TABLE(table), anim_add_button0, 0, 1, 0, 1);
+	gtk_table_attach_defaults(GTK_TABLE(table), anim_add_button1, 0, 1, 1, 2);
+	gtk_table_attach_defaults(GTK_TABLE(table), anim_add_button2, 0, 1, 2, 3);
+	
+	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(tmp_anim_scrolled), table);
+	
+	return tmp_anim_scrolled;
 }
 
 //    +-------------------------+
