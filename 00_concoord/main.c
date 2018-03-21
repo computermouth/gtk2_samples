f59d61bc1ef89a3247e7c8f5c5d36ecfc0789dd1
computermouth <computermouth@crunchbangplusplus.org>
2018-03-20 19:08:05 -0700

    Better scrolling windows

--- 00_concoord/main.c
+++ 00_concoord/main.c
@@ -5,6 +5,7 @@ static gpointer cc_new_window();
 static gpointer cc_new_anim_pane();
 static gpointer cc_new_anim_scrolled();
 static gpointer cc_new_frame_pane();
+static gpointer cc_new_frame_scrolled();
 static gpointer cc_new_canvas_pane();
 
 int main(int argc, char *argv[]){
@@ -64,8 +65,6 @@ static gpointer cc_new_anim_pane(){
 	
 	GtkWidget *tmp_anim_pane = gtk_hpaned_new();
 	
-	//~ GtkWidget *anim_add_button = gtk_button_new_with_label("Add Anim");
-	//~ gtk_widget_set_size_request(anim_add_button, 100, 500);
 	GtkWidget *anim_scrolled = cc_new_anim_scrolled();
 	GtkWidget *frame_pane = cc_new_frame_pane();
 	
@@ -93,7 +92,7 @@ static gpointer cc_new_anim_scrolled(){
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(tmp_anim_scrolled),
 		GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 	
-	GtkWidget *table = gtk_table_new(10, 1, TRUE);
+	GtkWidget *table = gtk_table_new(1024, 1, TRUE);
 	GtkWidget *anim_add_button0 = gtk_button_new_with_label("Anim");
 	GtkWidget *anim_add_button1 = gtk_button_new_with_label("Anim");
 	GtkWidget *anim_add_button2 = gtk_button_new_with_label("+");
@@ -108,7 +107,7 @@ static gpointer cc_new_anim_scrolled(){
 }
 
 //    +-------------------------+
-//    | + | + |                 |
+//    | +|| + |                 |
 //    |   |   |                 |
 //    |   |   |                 |
 //    |   |   |                 |
@@ -122,14 +121,45 @@ static gpointer cc_new_frame_pane(){
 	
 	GtkWidget *tmp_frame_pane = gtk_hpaned_new();
 	
-	GtkWidget *frame_add_button = gtk_button_new_with_label("Add FRAME");
-	gtk_widget_set_size_request(frame_add_button, 100, 500);
+	GtkWidget *frame_scrolled = cc_new_frame_scrolled();
 	GtkWidget *canvas_pane = cc_new_canvas_pane();
 	
-	gtk_paned_add1(GTK_PANED(tmp_frame_pane), frame_add_button);	
+	gtk_paned_add1(GTK_PANED(tmp_frame_pane), frame_scrolled);	
 	gtk_paned_add2(GTK_PANED(tmp_frame_pane), canvas_pane);
 	
 	return tmp_frame_pane;
+}
+
+//    +-------------------------+
+//    | +|| +||                 |
+//    |   |   |                 |
+//    |   |   |                 |
+//    |   |   |                 |
+//    |   |   |                 |
+//    |   |   |                 |
+//    |   |   |                 |
+//    |   |   |                 |
+//    +-------------------------+
+
+static gpointer cc_new_frame_scrolled(){
+	
+	GtkWidget *tmp_frame_scrolled = gtk_scrolled_window_new(NULL, NULL);
+	gtk_container_set_border_width(GTK_CONTAINER(tmp_frame_scrolled), 5);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(tmp_frame_scrolled),
+		GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	
+	GtkWidget *table = gtk_table_new(10, 1, TRUE);
+	GtkWidget *frame_add_button0 = gtk_button_new_with_label("Frame");
+	GtkWidget *frame_add_button1 = gtk_button_new_with_label("Frame");
+	GtkWidget *frame_add_button2 = gtk_button_new_with_label("+");
+	
+	gtk_table_attach_defaults(GTK_TABLE(table), frame_add_button0, 0, 1, 0, 1);
+	gtk_table_attach_defaults(GTK_TABLE(table), frame_add_button1, 0, 1, 1, 2);
+	gtk_table_attach_defaults(GTK_TABLE(table), frame_add_button2, 0, 1, 2, 3);
+	
+	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(tmp_frame_scrolled), table);
+	
+	return tmp_frame_scrolled;
 }
 
 //    +-------------------------+
